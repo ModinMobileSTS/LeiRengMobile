a3c1a508f0c09ce7a7d25211117a2058cb6c5ee1
ketal <k@ketal.icu>
2025-06-15 15:45:12 +0800

    fix: 修复使用 `啊搞得我想都想不到` 和 `哈里路小旋风` 时可能闪退

--- module/src/main/java/ninja/cards/ICantImagine.java
+++ module/src/main/java/ninja/cards/ICantImagine.java
@@ -1,7 +1,9 @@
 package ninja.cards;
 
-import static ninja.Ninja.getResourcePath;import ninja.Ninja;import ninja.enums.CardColorEnum;
+import ninja.Ninja;
+import ninja.enums.CardColorEnum;
 import ninja.powers.LexKela;
+
 import com.megacrit.cardcrawl.android.mods.abstracts.CustomCard;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
@@ -9,7 +11,7 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
-import com.megacrit.cardcrawl.monsters.AbstractMonster;import static ninja.Ninja.getResourcePath;import ninja.Ninja;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 
 public class ICantImagine extends CustomCard {
@@ -19,29 +21,32 @@ public class ICantImagine extends CustomCard {
     public static final String IMG_PATH = "img/cards_Ninja/ICantImagine.png";
     public static final String ID = "ICantImagine";
 
-    public ICantImagine(){
-        super (ID, NAME,Ninja.getResourcePath( IMG_PATH), 1, DESCRIPTION, CardType.SKILL, CardColorEnum.SILVER, CardRarity.COMMON, CardTarget.SELF);
+    public ICantImagine() {
+        super(ID, NAME, Ninja.getResourcePath(IMG_PATH), 1, DESCRIPTION, CardType.SKILL, CardColorEnum.SILVER, CardRarity.COMMON, CardTarget.SELF);
         this.baseMagicNumber = 1;
         this.magicNumber = this.baseMagicNumber;
         this.exhaust = true;
     }
 
-    public void use(AbstractPlayer p , AbstractMonster m){
+    public void use(AbstractPlayer p, AbstractMonster m) {
         CardCrawlGame.sound.play(ID);
         AbstractPower lexkela = p.getPower("LexKela");
-        int amount = this.magicNumber*lexkela.amount;
-        this.addToBot(new ApplyPowerAction(p , p ,new LexKela(p , -lexkela.amount),-lexkela.amount));
+        if (lexkela == null) {
+            return;
+        }
+        int amount = this.magicNumber * lexkela.amount;
+        this.addToBot(new ApplyPowerAction(p, p, new LexKela(p, -lexkela.amount), -lexkela.amount));
         this.addToBot(new GainEnergyAction(amount));
     }
 
-    public void upgrade(){
-        if(!this.upgraded){
+    public void upgrade() {
+        if (!this.upgraded) {
             this.upgradeName();
             this.upgradeMagicNumber(1);
         }
     }
 
-    public AbstractCard makeCopy(){
+    public AbstractCard makeCopy() {
         return new ICantImagine();
     }
 }
